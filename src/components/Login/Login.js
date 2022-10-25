e0fd494f85b9dd50b63403c21b6081552340525d
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-10-26 05:03:55 +0600

    Environment variables corrected

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,23 +2,42 @@ import React, { useContext } from 'react';
 import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 import { GoogleAuthProvider } from "firebase/auth";
+import { FacebookAuthProvider } from "firebase/auth";
 import { AuthContext } from '../../context/AuthProvider/AuthContext';
+import { FaGoogle } from "react-icons/fa";
 
 
-
-const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
     const { providerLogin } = useContext(AuthContext)
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     console.log(watch("example"));
+    const googleProvider = new GoogleAuthProvider();
+    const facebookProvider = new FacebookAuthProvider();
+
+
+    const handleGoogleLogin = () => {
+        providerLogin(googleProvider)
+            .then(result => {
+                const user = result.user;
+                // setUser(user)
+                console.log(user);
+            })
+            .catch((error) => {
+                console.error('error:', error);
+            })
+    }
+
+
     return (
         <div>
             <h1 className='mt-6 mb-7 text-4xl font-medium text-center text-indigo-600 mx-auto '>Login</h1>
-            {
-                <p>  Login with Google</p>
-            }
+
+            <button onClick={handleGoogleLogin} className='border border-indigo-600 flex px-7 py-2 text-xl rounded-lg text-indigo-600 font-medium mx-auto items-center'><FaGoogle className='mr-5' />Login with Google</button>
+            <button onClick={handleGoogleLogin} className='border border-indigo-600 flex px-7 py-2 text-xl rounded-lg text-indigo-600 font-medium mx-auto items-center'><FaGoogle className='mr-5' />Login with Google</button>
+
+
             <form className='flex flex-col ' onSubmit={handleSubmit(onSubmit)}>
                 <input className='mt-6 border mx-auto border-indigo-600' type="email" placeholder="Email" {...register("Email", { required: true })} />
                 {errors.Email?.type === 'required' && <p className='text-center text-xl text-orange-500' role="alert">Email is required</p>}
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,23 +4,23 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_apiKey,
-//     authDomain: process.env.REACT_APP_authDomain,
-//     projectId: process.env.REACT_APP_projectId,
-//     storageBucket: process.env.REACT_APP_storageBucket,
-//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//     appId: process.env.REACT_APP_appId
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBqIA-1CzGwJzz4CIMhqymsuWj6C1_Hjr4",
-    authDomain: "assignment-10-6cf4d.firebaseapp.com",
-    projectId: "assignment-10-6cf4d",
-    storageBucket: "assignment-10-6cf4d.appspot.com",
-    messagingSenderId: "83572318376",
-    appId: "1:83572318376:web:5c686e4b64213f7a1f229d"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBqIA-1CzGwJzz4CIMhqymsuWj6C1_Hjr4",
+//     authDomain: "assignment-10-6cf4d.firebaseapp.com",
+//     projectId: "assignment-10-6cf4d",
+//     storageBucket: "assignment-10-6cf4d.appspot.com",
+//     messagingSenderId: "83572318376",
+//     appId: "1:83572318376:web:5c686e4b64213f7a1f229d"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
