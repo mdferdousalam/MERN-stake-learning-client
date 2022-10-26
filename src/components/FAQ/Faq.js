c3af17785f5ca6e93f5a56f1f98731b4627a848a
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-10-26 19:12:12 +0600

    Faq added

--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -3,7 +3,59 @@ import React from 'react';
 const Faq = () => {
     return (
         <div>
-            <h2>Faq here</h2>
+            <h2> Common questions about for a Programmer</h2>
+            <div>
+                <h1>Is there a difference between a coder and a programmer?</h1>
+                <p>Although the main activity for both coders and programmers is coding, programmers typically have a more complex set of tasks they need to perform. Coders usually solely write application code in the programming language they are instructed to, whereas programmers also need to create software functions, develop automation logic and solve various problems.</p>
+            </div>
+            <div>
+                <h1>What skills do programmers need?</h1>
+                <p>The most important skills for a programmer are:
+
+                    Mathematical skills: A basic understanding of both arithmetic and algebra is essential for programming.
+                    Problem-solving: A significant amount of a programmer's time is spent on solving various issues with the software.
+                    Communication: Most software development jobs are team efforts, so programmers need to be able to successfully communicate with other team members.
+                    Computer skills: Most programmers have computer knowledge beyond the programming languages they routinely use.</p>
+            </div>
+            <div>
+
+            </div>
+            <div>
+                <h1>What is MERN Stack?</h1>
+                <p>The MERN stack is the most popular stacks used to create dynamic web interfaces. It’s composed of MongoDB, Express.js, React, and Node.js. The MERN stack is majorly used in social media platforms and news aggregation applications. Prominent companies like Netflix and Uber has been using MERN stack in their web development.
+
+                    React is the most popular JavaScript library and it is primarily used to build single-page applications and mobile applications. React makes it simple to create and manage the user interface (UI) of an application.
+
+                    When put together with MongoDB, Express.js, and Node.js, React gives you a JavaScript-based, developer-friendly and cost-effective tech stack.</p>
+            </div>
+            <h1>Steps to become a MERN Stack Developer</h1>
+            <div>
+                <ul>
+                    <li><h1>Learn Front-End Programming Languages</h1></li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Learn Using Front End Tools</li>
+                    <li>Understand the Basics of Website Design</li>
+                    <li>Learn React</li>
+                    <li>Learn Back End Components of MERN Stack</li>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>MongoDB</li>
+                </ul>
+
+
+            </div>
+            <div><h1>Roadmap of front end Developer</h1>
+                <img src="https://scontent.fdac24-2.fna.fbcdn.net/v/t39.30808-6/312106661_2162617270741788_1386475972978583823_n.jpg?stp=dst-jpg_p843x403&_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGRj5YjVJkHLrQJcckSSL_5sFtF7SBTCjawW0XtIFMKNs6nAJOu8m8fchayGW8I99ZXj4x6LUUjJvsW8Nix-sMp&_nc_ohc=X5lfam8Ksx8AX-Te6ro&tn=Ze0gaH_Il7KNw1h4&_nc_pt=1&_nc_ht=scontent.fdac24-2.fna&oh=00_AT9qPHR1x4dzGxeyCuJgYtbLpWPUnw70YQpDSg7uvoegoQ&oe=635DB403" alt="" />
+            </div>
+
+            <div>
+                <h1>Roadmap of Backend Technology</h1>
+                <img src="https://scontent.fdac24-2.fna.fbcdn.net/v/t39.30808-6/312480615_2162616874075161_5961619297278198756_n.jpg?stp=dst-jpg_p843x403&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeH14_B1cRzAhgv6feErFGyYq2-26PsHzRarb7bo-wfNFjb2UrjQLpebE2xI8NMh5PxKwO-bTRFeGpgP5bg5gqdj&_nc_ohc=rcyUS16WGnIAX9bCiOg&tn=Ze0gaH_Il7KNw1h4&_nc_pt=1&_nc_ht=scontent.fdac24-2.fna&oh=00_AT8WMBoRtL_Zx8CIbppCWfeOVNqRRFA-92AEX4_waQIEtg&oe=635EAF20" alt="" />
+            </div>
+            <div></div>
+            <div></div>
         </div>
     );
 };
